7ef67082f57eb7581dd7b3d2dcb1810f48af22ed
jkcommunicationcrafts <jatin.k@communicationcrafts.com>
2025-07-07 18:10:51 +0530

    setup added

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Button variant="destructive">Click Me</Button>
+    </div>
+  )
+}
+
+export default page
