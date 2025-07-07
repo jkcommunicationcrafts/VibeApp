5620c0222c8f9747cbec8c8f286e347efd8da06a
jkcommunicationcrafts <jatin.k@communicationcrafts.com>
2025-07-07 18:21:00 +0530

    01: setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <Button variant="destructive">Click Me</Button>
+      <Button variant="outline">Click Me</Button>
     </div>
   )
 }
