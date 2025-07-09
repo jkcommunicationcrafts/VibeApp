542f6f083ee25f6fc98efa460be27cc68204dca9
jkcommunicationcrafts <jatin.k@communicationcrafts.com>
2025-07-09 12:15:50 +0530

    05-AI-Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
 
-import React from "react";
+import React, { useState } from "react";
 
 const Page = () => {
-  const trpc= useTRPC()
-  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+  const trpc = useTRPC();
+  const [value, setvalue] = useState("");
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
   return (
     <div className="p-4 max-w-2xl mx-auto">
-      <Button onClick={()=>invoke.mutate({text:"Jatin"})}>Invoke Background job</Button>
+      <Input value={value} onChange={(e) => setvalue(e.target.value)} className="mb-5"/>
+      <Button onClick={() => invoke.mutate({ input: value })}>
+        Invoke Background job
+      </Button>
     </div>
   );
 };
